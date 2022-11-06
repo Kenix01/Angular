268aeb1b230a9e01b7fca9e6a7508d827f9427b9
Kenix01 <i.i.stefanyuk@gmail.com>
2022-11-06 18:14:18 +0200

    lesson_4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/configs/urls.ts
@@ -0,0 +1,18 @@
+import {environment} from "../../environments/environment";
+
+const {API} = environment;
+
+const urls = {
+  cars:`${API}/cars`
+}
+
+export {
+  urls
+}
+
+
+
+
+
+
+
